d1321f9f6903e5e50d3e1ff409b150958ee50237
MarkoAyala <markoayala3@hotmail.com>
2022-07-27 16:21:23 -0300

    .env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require('./src/app.js');
 
+
+
 const PORT = process.env.PORT || 3001
   server.listen(PORT , ()=>{
-    console.log(`Levantado en el puerto ${PORT}`)
+    console.log(`Levantado en el puerto ${process.env.PORT}`)
   });
 
 
