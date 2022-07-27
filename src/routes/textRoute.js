19a35aa3e0f5f0001d323148bc3b8d085406d0d4
MarkoAyala <markoayala3@hotmail.com>
2022-07-27 10:55:32 -0300

    in heroku

--- src/routes/textRoute.js
+++ src/routes/textRoute.js
@@ -4,7 +4,6 @@ server.get('/',  async (req,res,_next) => {
     try {
         const {text} = req.query;
         const reverse = text.split('').reverse().join("");
-        console.log(typeof reverse)
         if(reverse === text){
             return res.status(200).json([text]);
         }
